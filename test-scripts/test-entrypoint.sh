aef374114169b55f701b6fe12aa7320ea1f170b5
Caspian Baska <email@caspian.computer>
2021-11-10 18:00:00 +1100

    fix: silence negative output of shard check

--- test-scripts/test-entrypoint.sh
+++ test-scripts/test-entrypoint.sh
@@ -34,7 +34,7 @@ do
   esac
 done
 
-shards check --ignore-crystal-version || shards install --ignore-crystal-version
+shards check --ignore-crystal-version -q &> /dev/null || shards install --ignore-crystal-version
 
 if [[ "$watch" == "true" ]]; then
   CRYSTAL_WORKERS=$(nproc) watchexec -e cr -c -r -w src -w spec -- scripts/crystal-spec.sh -v $PARAMS
